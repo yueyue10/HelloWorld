023d13ed6b4e2c2a987594863a690ab1a0a0efe6
yueyue10 <1650432983@qq.com>
2015-11-03 17:23:14 +0800

    在MainActivity中增加TextView的内容。

--- app/src/main/java/com/alibaba/com/helloworld/MainActivity.java
+++ app/src/main/java/com/alibaba/com/helloworld/MainActivity.java
@@ -4,14 +4,18 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private TextView hello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hello = (TextView) findViewById(R.id.hello_world);
     }
 
 
