b958d9b78fb84e09e5480992e61840a8cd45f529
yueyue10 <1650432983@qq.com>
2015-11-03 17:29:15 +0800

    增加SecondActivity

---
+++ app/src/main/java/com/alibaba/com/helloworld/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.alibaba.com.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by 0 on 2015/11/3.
+ */
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+    }
+}
