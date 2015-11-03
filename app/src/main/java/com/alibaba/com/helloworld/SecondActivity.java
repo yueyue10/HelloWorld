e505e90087966ccfcd5bc3dfe77cc7bff831520c
yueyue10 <passionyueyue@sina.com>
2015-11-03 23:01:49 +0800

    在SecondActivity中新增TextView second、

--- app/src/main/java/com/alibaba/com/helloworld/SecondActivity.java
+++ app/src/main/java/com/alibaba/com/helloworld/SecondActivity.java
@@ -7,11 +7,12 @@ import android.os.Bundle;
  * Created by 0 on 2015/11/3.
  */
 public class SecondActivity extends Activity {
-
+	
+	private TextView second;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-
+		second=(TextView)findViewById(R.id.second_activity);
     }
 }
